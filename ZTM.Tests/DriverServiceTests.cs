917749643457468c7d06478370adf5b66b92fad7
Adrian Bloch <s21488@pjwstk.edu.pl>
2022-07-02 18:14:56 +0200

    WithTest

---
+++ ZTM.Tests/DriverServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Xunit;
+using ZTM.CORE.DTO.Services;
+using ZTM.Infrastructure.Repository;
+
+namespace ZTM.Tests;
+
+public class DriverServiceTests
+{
+    
+    [Fact]
+    public async Task ShouldReturnGivenNameAndSurname()
+    {
+        var driver = new DriverService(Mock.Of<IDriverRepository>());
+        var result = await driver.GetAllDriversBasicInfoAsync();
+        result.Should().BeEmpty();
+    }
+}
